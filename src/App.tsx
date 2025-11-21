70b9a379a5ed8c5c4d18e5ad6dfe432788f780f5
Ivan Hudko <giv.10072007@gmail.com>
2025-11-21 16:22:52 +0200

    add InputComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-
+import Input from "./components/input/Input"
+import styles from "./Main.module.css";
 function App() {
 
 
   return (
-    <></>
+    <div className={styles.inputContainer}>
+      <Input type="password"/>
+      <Input type="number"/>
+      <Input type="text"/>
+      <Input type="password" label="Password" clearable/>
+      <Input type="number" label="Number " clearable/>
+      <Input type="email" label="Email" placeholder="example@email.com" clearable/>
+    </div>
   )
 }
 
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,6 +1,79 @@
+import { useState } from "react";
+import { LuEye, LuEyeOff } from "react-icons/lu";
+import { IoMdClose } from "react-icons/io";
 
-const Input : React.FC = ()=>{
-return <></>
+import styles from "./Input.module.css";
+
+interface Props {
+    type: "password" | "text" | "email" | "number";
+    clearable?: boolean;
+    label?: string;
+    placeholder?: string;
+}
+
+const Input: React.FC<Props> = ({
+    type = "text",
+    clearable = false,
+    label,
+    placeholder = "",
+}) => {
+    const [value, setValue] = useState<string>("");
+    const [inputType, setInputType] = useState<Props["type"]>(type);
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>){
+        const val = e.target.value;
+    
+        if (type === "number") {
+            if (/^-?\d*\.?\d*$/.test(val)) {
+                setValue(val);
+            }
+            return;
+        }
+
+        setValue(val);
+    };
+
+    function  handleClear(){
+        setValue("");
+    };
+
+    function togglePasswordVisibility(){
+        setInputType((prev) => (prev === "password" ? "text" : "password"));
+    };
+
+    return (
+        <div className={styles.wrapper}>
+            {label && <label>{label}</label>}
+
+            <div className={styles.inputWrapper}>
+                <input
+                    type={inputType}
+                    value={value}
+                    onChange={handleChange}
+                    className={styles.input}
+                    placeholder={placeholder}
+                />
+
+                <div className={styles.controls}>
+                    {clearable && value.length > 0 && (
+                        <button type="button" onClick={handleClear} className={styles.clearBtn}>
+                            <IoMdClose />
+                        </button>
+                    )}
+
+                    {type === "password" && (
+                        <button
+                            type="button"
+                            onClick={togglePasswordVisibility}
+                            className={styles.visibilityBtn}
+                        >
+                            {inputType === "password" ? <LuEye /> : <LuEyeOff />}
+                        </button>
+                    )}
+                </div>
+            </div>
+    </div>
+);
 }
 
 export default Input;
